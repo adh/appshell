affe56115f76e82640cfdcd4e5dc5212c7711690
Ales Hakl <ales@hakl.net>
2015-02-15 06:22:55 +0100

    basic setuptools infrastructure

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "appshell",
+    version = "0.0.1",
+    author = "Ales Hakl",
+    author_email = "andrewjcarter@gmail.com",
+    description = ("Bootstrap and Flask based application framework"),
+    license = "MIT",
+    keywords = "flask bootstrap admin",
+#    url = "",
+    packages=['appshell'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: Flask"
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ],
+    install_requirs = [
+        "Flask",
+        "Flask-BabelEx",
+        "Flask-Bootstrap",
+        "Flask-Login",
+        "Flask-SQLAlchemy",
+        "Flask-WTF",
+        "WTForms-Alchemy"
+    ]
+)
